a0521864794fe8f8b8508490592f9e56ed346c2d
GioKas93 <giokas93@pm.me>
2025-01-18 20:09:14 +0100

    move to custom extras folder
    Change-Id: Ib98ea108875b1ecf9afedf3b0cf82e09d4f3b7e4

--- config/common.mk
+++ config/common.mk
@@ -1,5 +1,5 @@
 # Allow vendor/extra to override any property by setting it first
-$(call inherit-product-if-exists, vendor/extra/product.mk)
+$(call inherit-product-if-exists, vendor/extras/lineage.mk)
 
 PRODUCT_BRAND ?= LineageOS
 
