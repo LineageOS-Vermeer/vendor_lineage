c0d97e206c091057650bbbe607b69a35d30862a5
GioKas93 <giokas93@pm.me>
2025-01-21 19:24:41 +0100

    inherit gapps
    Change-Id: I2120b69c3cc8ad6c415fc0b59ea6602556f22615

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,12 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extras/lineage.mk)
 
+# Inherit gapps packages
+ifeq ($(WITH_GMS),true)
+$(call inherit-product, vendor/google/gms/config.mk)
+$(call inherit-product, vendor/google/pixel/config.mk)
+endif
+
 PRODUCT_BRAND ?= LineageOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
@@ -235,11 +241,6 @@ ifeq ($(TARGET_BUILD_VARIANT),userdebug)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     debug.sf.enable_transaction_tracing=false
 endif
-
-# SetupWizard
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.theme=glif_v4 \
-    setupwizard.feature.day_night_mode_enabled=true
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/no-rro
 PRODUCT_PACKAGE_OVERLAYS += \
