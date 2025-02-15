8369997934821f06c9cde503b7d3a4fb1cde6692
Danny Lin <danny@kdrag0n.dev>
2025-02-15 18:56:13 +0800

    common: Disable touch video heatmap
    Change-Id: Iadec34fe1eb574bf40cf4a1f849772e5f64ee8db
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -159,6 +159,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.graphics.game_default_frame_rate.disabled=true
 
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
+
 # Extra tools in Lineage
 PRODUCT_PACKAGES += \
     bash \
